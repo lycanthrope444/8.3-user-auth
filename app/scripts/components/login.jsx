70cda657bac66ad88de62237840c8557dcd86800
Nathan <nathanstarwalt@gmail.com>
2017-03-01 15:03:40 -0500

    Component functionality implemented for login screen

--- app/scripts/components/login.jsx
+++ app/scripts/components/login.jsx
@@ -6,12 +6,18 @@ class LogIn extends React.Component{
   constructor(){
     super();
   }
+  userLogin(email, password){
+    console.log('user Login clicked', email, password);
+  }
+  createUser(email, password){
+    console.log('user create clicked', email, password);
+  }
   render(){
     return(
       <div className="container">
         <div className="row">
-          <UserLogin />
-          <CreateUser />
+          <UserLogin data={this.state} userLogin={this.userLogin}/>
+          <CreateUser data={this.state} createUser={this.createUser}/>
         </div>
       </div>
     )
@@ -19,59 +25,82 @@ class LogIn extends React.Component{
 }
 
 class UserLogin extends React.Component{
-    constructor(props){
-      super(props);
+  constructor(props){
+    super(props);
 
-    }
-    render(){
-      return(
-       <div className="col-md-6">
-         <h1>Please Login</h1>
+  }
+  render(){
+    return(
+     <div className="col-md-6">
+       <h1>Please Login</h1>
 
-         <form id="login">
-           <div className="form-group">
-             <label htmlFor="email-login">Email address</label>
-             <input className="form-control" name="email" id="email-login" type="email" placeholder="email" />
-           </div>
+       <form id="login">
+         <div className="form-group">
+          <label htmlFor="email-login">Email address</label>
+          <input className="form-control" name="email"
+            id="email-login" type="email" placeholder="email"
+          />
+         </div>
 
-           <div className="form-group">
-             <label htmlFor="password-login">Password</label>
-             <input className="form-control" name="password" id="password-login" type="password" placeholder="Password Please" />
-           </div>
+         <div className="form-group">
+          <label htmlFor="password-login">Password</label>
+          <input className="form-control"
+            name="password" id="password-login"
+            type="password" placeholder="Password Please"
+          />
+         </div>
 
-           <input className="btn btn-primary" type="submit" value="Beam Me Up!" />
-         </form>
-       </div>
-      )
-    }
+        <input className="btn btn-primary" type="submit" value="Beam Me Up!"
+          onClick = {
+            (e)=>{
+              e.preventDefault(); this.props.userLogin($('#email-login').val(), $('#password-login').val())
+            }
+          }
+        />
+       </form>
+     </div>
+    )
+  }
 }
 
 class CreateUser extends React.Component{
-    constructor(props){
-      super(props);
+  constructor(props){
+    super(props);
 
-    }
-    render(){
-      return(
-        <div className="col-md-6">
-          <h1>No Account? Sign Up!</h1>
+  }
+  render(){
+    return(
+      <div className="col-md-6">
+        <h1>No Account? Sign Up!</h1>
 
-          <form id="signup">
-            <div className="form-group">
-              <label htmlFor="signup-email">Email</label>
-              <input id="signup-email" className="form-control" type="text" name="email" placeholder="Email Address" />
-            </div>
+        <form id="signup">
+          <div className="form-group">
+            <label htmlFor="signup-email">Email</label>
+            <input id="signup-email" className="form-control" type="text"
+              name="email" placeholder="Email Address"
+            />
+          </div>
 
-            <div className="form-group">
-              <label htmlFor="signup-password">Password</label>
-              <input id="signup-password" className="form-control" type="password" name="password" placeholder="Don't Share This!" />
-            </div>
+          <div className="form-group">
+            <label htmlFor="signup-password">Password</label>
+            <input id="signup-password" className="form-control"
+              type="password" name="password"
+              placeholder="Don't Share This!"
+            />
+          </div>
 
-            <input className="btn btn-primary" type="submit" value="Sign Up!" />
-          </form>
-        </div>
-      )
-    }
+          <input className="btn btn-primary" type="submit" value="Sign Up!"
+            onClick = {
+              (e)=>{
+                e.preventDefault();
+                this.props.createUser($("#signup-email").val(), $('#signup-password').val())
+              }
+            }
+          />
+        </form>
+      </div>
+    )
+  }
 }
 
 
--- app/scripts/models/models.js
+++ app/scripts/models/models.js
@@ -1,2 +1,25 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
+
+var User = Backbone.Models.extend({
+  id:'_id'
+});
+
+var UserCollection = Backbone.Collection.extend({
+  model: user
+});
+
+var MessagesModel = Backbone.Model.extend({
+  id:'_id'
+});
+
+var MessagesCollection = Backbone.Collection.extend({
+  model: MessagesModel
+});
+
+module.exports = {
+  User: User,
+  UserCollection: UserCollection,
+  MessagesModel: MessagesModel,
+  MessagesCollection: MessagesCollection
+};
